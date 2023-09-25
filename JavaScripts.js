bf1083c78a4c4afdcf39f84b28d0ab2a53c23672
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-09-25 10:07:33 +0200

    functions och json

--- JavaScripts.js
+++ JavaScripts.js
@@ -1,9 +1,24 @@
+let json = [
+    
+]
+
+
 function init(){
 
+    for(i = 0; i < 5; i++){
+    days();
+    }
 }
 window.onload = init;
 
 
-for(i = 0; i < 5; i++){
+function days(){
+    //en if sats för att kolla hur många maträtter det är per dag.
 
+}
+
+
+function create(){
+    //skapa P tag.
+    //Skapa span i P tag
 }
