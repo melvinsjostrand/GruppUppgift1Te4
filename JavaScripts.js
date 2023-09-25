306f8bd2c1c524278476b1faa9d76084053473d8
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-09-25 16:56:53 +0200

    3 luncher per dag

--- JavaScripts.js
+++ JavaScripts.js
@@ -8,20 +8,18 @@ let index = 1;
 
 function init(){
     week();
-    CreateSection();
-    createLunch();
     for(i = 0; i < 5; i++){
-    days();
+        createLunch();
     }
 }
 window.onload = init;
 
 
-function days(){
+/*function days(){
     for(Element in JSON){
         createLunch();
     }
-}
+}*/
 
 function week(){
     let h2 = document.getElementsByTagName("h2");
@@ -30,31 +28,38 @@ function week(){
     //funkar att visa vecka.
 }
 
-function CreateSection(){
-
+function createLunch(){
+    let P;
+    let span;
+    let text = [
+        "Moussaka Grekisk potatis- och grönsaksgratäng på ärtfärs serveras med salladsbuffé" ,  
+        "Potatis- och zucchiniplättar serveras med rårörda lingon och salladsbuffé",
+        "Spaghetti Bolognese Pastasås på nötfärs serveras med salladsbuffé"
+        ]
    
-}
-
-
-function createLunch(CreateSection){
-   for(i = 0; i < 5; i++){
     let matsedel = document.getElementById("matsedel");
     let section = document.createElement("section");
     let h3 = document.createElement("h3");
     let div = document.createElement("div");
-    let P = document.createElement("p");
-    let span = document.createElement("span");
-
-    section.appendChild(h3);
-    matsedel.appendChild(section);
-    section.appendChild(div);
-    div.appendChild(P);
-    P.appendChild(span);
-    h3.innerText = day[i];
-    span.innerText = "lunch" + index++;
-    P.innerText += "Moussaka Grekisk potatis- och grönsaksgratäng på ärtfärs serveras med salladsbuffé";
+        for(l = 1; l < 4; l++){
+           
+            span = document.createElement("span");
+            P = document.createElement("p");
+            div.appendChild(P);  
+           
+            P.appendChild(span);
+            span.innerText = "Lunch " + l;
+             P.innerText += text[1];
+            console.log(span);
+         
+    
+        }
+            section.appendChild(h3);
+            matsedel.appendChild(section);
+            section.appendChild(div);
+            h3.innerText = day[i];
     }
-}
+
 
 async function getjson(){
     let path = ""
